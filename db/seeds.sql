fe8d59a463050c8edb1b906f20fc75e56a5f97a0
Wehbiels <Lara.Wehbie@gmail.com>
2018-04-27 23:42:39 -0400

    db add

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Tripple Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Venison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lobster Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
