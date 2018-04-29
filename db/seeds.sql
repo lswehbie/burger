c6f4021b214be3eebdc07fbd49feda9c1e0449dd
Wehbiels <Lara.Wehbie@gmail.com>
2018-04-28 23:08:29 -0400

    burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO burgers (burger_name) VALUES ('Tripple Cheese Burger');
-INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Venison Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Lobster Burger', true);
-INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Venison Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Lobster Burger', true);
+
